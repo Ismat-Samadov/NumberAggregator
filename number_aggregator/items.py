f300a1bc2ebe8ea3e10472c1b086f79b301152a5
Ismat <ismetsemedov@gmail.com>
2024-12-27 20:18:03 +0400

    setup

--- number_aggregator/items.py
+++ number_aggregator/items.py
@@ -1,12 +1,24 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
+# items.py
+from scrapy import Item, Field
 
-import scrapy
-
-
-class NumberAggregatorItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class AutoNetItem(Item):
+    car_id = Field()
+    title = Field()
+    price = Field() 
+    engine_capacity = Field()
+    year = Field()
+    mileage = Field()
+    make = Field()
+    model = Field()
+    city = Field()
+    color = Field()
+    barter = Field()
+    credit = Field()
+    phone1 = Field()
+    phone2 = Field()
+    is_salon = Field()
+    transmission = Field()
+    drive_type = Field()
+    description = Field()
+    created_at = Field()
+    raw_data = Field()
